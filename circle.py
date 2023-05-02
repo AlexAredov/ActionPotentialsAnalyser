dcd3bdefdefa31595148875fda16302189a09195
Alex Aredov <aaredov@bk.ru>
2023-05-02 14:59:11 +0300

    Lib

--- circle.py
+++ circle.py
@@ -2,8 +2,25 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from math import sqrt
+from scipy.ndimage import gaussian_filter
+from io import StringIO
+from skimage.restoration import denoise_tv_chambolle
+import numpy as np
 
-def circle(file):
+def preprocess(file, smooth = 15):
+    with open("./" + file, 'r') as file:
+        file_content = file.read()
+    file_content = file_content.replace(',', '.')
+
+    with StringIO(file_content) as file_buffer:
+        data = np.genfromtxt(file_buffer, delimiter='\t')
+
+    time = data[:, 0] * 1000
+    voltage = denoise_tv_chambolle((data[:, 1] * 1000), smooth)
+    voltage = gaussian_filter(voltage, 1)
+    return time, voltage
+
+def circle(time, voltage):
     #grid
     plt.style.use('seaborn-whitegrid')
 
@@ -39,38 +56,31 @@ def circle(file):
 
         x_r = (b2-b1)/(k2-k1)
         y_r = -k1*x_r + b1
-        print(x_r, y_r)
+        #print(x_r, y_r)
 
         rad = sqrt((x_r - x_c)**2 + (y_r - y_c)**2)
-        print(rad)
+        #print(rad)
 
         plt.gca().add_patch(plt.Circle((x_r, y_r), rad, color='lightblue', alpha=0.5))
         return rad, x_r, y_r
 
-    #open file
-    #file = open('./11.txt')
-    x_t = []
-    y_t = []
-    for i in file:
-        x_temp = float(i.split('\t')[0].replace('\n', '').replace(',', '.'))
-        y_temp = float(i.split('\t')[1].replace('\n', '').replace(',', '.'))
-        x_t.append(x_temp)
-        y_t.append(y_temp)
+    x_t = list(time)
+    y_t = list(voltage)
 
     #writing just the right values
     x = []
     y = []
     for i in range(len(x_t)):
-        if (x_t[i] < x_t[y_t.index(max(y_t))]) and (y_t[i] < min(y_t)/2):
+        if (x_t[i] < x_t[list(y_t).index(max(y_t))]) and (y_t[i] < min(y_t)/2):
             x.append(x_t[i])
             y.append(y_t[i])
-    dat = {'x': x, 'y': y}
+    dat = {'x': x_t, 'y': y_t}
     datafr = pd.DataFrame(data=dat)
 
     #first flatting (the big one)
     l = 32
     dff = flat(x, y, 32)
-    ma = nearest_value(dff[0], dff[1], x[y.index(max(y))], min(y))
+    ma = nearest_value(dff[0], dff[1], x[list(y).index(max(y))], min(y))
 
     #number of dots to the left and right of the center
     o = 10
@@ -79,10 +89,10 @@ def circle(file):
     while ma + o >= len(dff[0]):
         l = l//2
         dff = flat(x, y, l)
-        ma = nearest_value(dff[0], dff[1], x[y.index(max(y))], min(y))
-    while dff[1][ma+o] - dff[1][ma] >= 0.003:
+        ma = nearest_value(dff[0], dff[1], x[list(y).index(max(y))], min(y))
+    while dff[1][ma+o] - dff[1][ma] >= 3:
         o -= 1
-        ma = nearest_value(dff[0], dff[1], x[y.index(max(y))], min(y))
+        ma = nearest_value(dff[0], dff[1], x[list(y).index(max(y))], min(y))
 
     #set the size of graph
     f = plt.figure()
@@ -90,24 +100,21 @@ def circle(file):
     f.set_figheight(5)
 
     #the number of flatting
-    print(l)
+    #print(l)
 
     #the dot (nearest to center)
-    plt.scatter(x[y.index(max(y))], min(y), color='orange', s=40, marker='o')
+    #plt.scatter(x[list(y).index(max(y))], min(y), color='orange', s=40, marker='o')
 
     #other dots
-    plt.scatter(dff[0][ma], dff[1][ma], color='green', s=40, marker='o')
-    plt.scatter(dff[0][ma+o], dff[1][ma+o], color='green', s=40, marker='o')
-    plt.scatter(dff[0][ma-o], dff[1][ma-o], color='green', s=40, marker='o')
+    #plt.scatter(dff[0][ma], dff[1][ma], color='green', s=40, marker='o')
+    #plt.scatter(dff[0][ma+o], dff[1][ma+o], color='green', s=40, marker='o')
+    #plt.scatter(dff[0][ma-o], dff[1][ma-o], color='green', s=40, marker='o')
 
     #finding the radius
     rad, x_r, y_r = radius(dff[0][ma], dff[1][ma], dff[0][ma+o], dff[1][ma+o], dff[0][ma-o], dff[1][ma-o])
 
     #drauing the rigth and flat graphes
-    plt.plot(datafr.x, datafr.y)
-    plt.plot(dff[0], dff[1])
+    #plt.plot(datafr.x, datafr.y)
 
-    plt.show()
+    #plt.show()
     return rad, x_r, y_r
-
-radius, x, y = circle(open('./11.txt'))
