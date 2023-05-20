d061b7bdd82b6596f497b1c9f2ead8eaf41409ab
Alex Aredov <aaredov@bk.ru>
2023-05-20 19:30:45 +0300

    optim

--- circle.py
+++ circle.py
@@ -22,56 +22,50 @@ def preprocess(file, smooth = 15):
 
 def circle(time, voltage):
     def nearest_value(items_x, items_y, value_x, value_y):
-        l = []
-        for i in range(len(items_x)):
-            l.append(sqrt((value_x - items_x[i]) ** 2 + (value_y - items_y[i]) ** 2))
-        return (l.index(min(l)))
+        return np.argmin(np.sqrt((items_x - value_x) ** 2 + (items_y - value_y) ** 2))
 
     def flat(x, y, n):
-        del x[::n]
-        del y[::n]
-        dat = [x, y]
-        return dat
+        return x[::n], y[::n]
 
     def radius(x_c, y_c, x_1, y_1, x_2, y_2):
-        cent1_x = (x_c + x_1) / 2
-        cent1_y = (y_c + y_1) / 2
-        cent2_x = (x_c + x_2) / 2
-        cent2_y = (y_c + y_2) / 2
 
-        k1 = (cent1_x - x_c) / (cent1_y - y_c)
-        b1 = cent1_y + k1 * cent1_x
+        k1 = ((x_c + x_1) / 2 - x_c) / ((y_c + y_1) / 2 - y_c)
+        b1 = (y_c + y_1) / 2 + ((x_c + x_1) / 2 - x_c) / ((y_c + y_1) / 2 - y_c) * (x_c + x_1) / 2
 
-        k2 = (cent2_x - x_c) / (cent2_y - y_c)
-        b2 = cent2_y + k2 * cent2_x
+        k2 = ((x_c + x_2) / 2 - x_c) / ((y_c + y_2) / 2 - y_c)
+        b2 = (y_c + y_2) / 2 + k2 * (x_c + x_2) / 2
 
         x_r = (b2 - b1) / (k2 - k1)
         y_r = -k1 * x_r + b1
 
-        rad = sqrt((x_r - x_c) ** 2 + (y_r - y_c) ** 2)
+        rad = np.sqrt((x_r - x_c) ** 2 + (y_r - y_c) ** 2)
         return rad, x_r, y_r
 
-    x = list(time)
-    y = list(voltage)
+    x = np.array(time)
+    y = np.array(voltage)
 
-    l = 32
-    dff = flat(x, y, 32)
-    ma = nearest_value(dff[0], dff[1], x[list(y).index(max(y))], min(y))
+    l:int = 32
+    dff = flat(x, y, l)
+    ma:float = nearest_value(dff[0], dff[1], x[np.argmax(y)], np.min(y))
 
-    o = 10
+    o:int = 10
 
+    # очень по гейски но что имеем
     while ma + o >= len(dff[0]):
         l = l // 2
+        if l == 0:  # проверка на ноль и замена нуля на 1
+            return 10, -10, 0
+
         dff = flat(x, y, l)
-        ma = nearest_value(dff[0], dff[1], x[list(y).index(max(y))], min(y))
-    
+        ma:float = nearest_value(dff[0], dff[1], x[np.argmax(y)], np.min(y))
+
     while dff[1][ma + o] - dff[1][ma] >= 8:
         if (dff[1][ma + o] - dff[1][ma]) / (dff[1][ma + (o - 1)] - dff[1][ma]) > 3.5:
             o -= 1
-            ma = nearest_value(dff[0], dff[1], x[list(y).index(max(y))], min(y))
+            ma = nearest_value(dff[0], dff[1], x[np.argmax(y)], np.min(y))
             break
         o -= 1
-        ma = nearest_value(dff[0], dff[1], x[list(y).index(max(y))], min(y))
+        ma:float = nearest_value(dff[0], dff[1], x[np.argmax(y)], np.min(y))
 
     rad, x_r, y_r = radius(dff[0][ma], dff[1][ma], dff[0][ma + o], dff[1][ma + o], dff[0][ma - o], dff[1][ma - o])
     return rad, x_r, y_r
