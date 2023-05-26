dd21501d5a80f11770b9630a23ad7d4a5d578431
Zakharov Oleg <dooggest@gmail.com>
2023-05-26 18:43:23 +0300

    спасибо илья

---
+++ Potentials/bin/Debug/PythonScripts/Save_APs_to_C#.py
@@ -0,0 +1,90 @@
+import atypic_cardio_analyzer as can
+import os
+import sys
+import shutil
+import warnings
+import math
+from concurrent.futures import ProcessPoolExecutor
+import dask.dataframe as dd
+
+# ----------------------------------------------------------------------------------------------
+
+C_sharp_data = sys.argv[1]
+
+lines = C_sharp_data.split('\n')
+warnings.filterwarnings("ignore")
+
+file = lines[0]
+inp_alpha_threshold = float(lines[1].replace(',', '.'))
+inp_start_offset = int(lines[2])
+inp_refractory_period = int(lines[3])
+limit_rad = float(lines[4].replace(',', '.'))
+
+separating_folder = "separated_APs"
+
+file_folder = os.path.dirname(file)  # получаем путь до папки перед файлом
+separating_path = os.path.join(file_folder, separating_folder).replace('\\', '/')  # добавляем separating_folder в конец
+
+time, voltage = can.preprocess(file, step=4)
+action_potentials = can.find_action_potentials(time, voltage, alpha=inp_alpha_threshold,
+                                            refractory_period=inp_refractory_period, offset=inp_start_offset)
+
+# <
+# Сохраняем каждые ПД в папку и получаем временные интервалы
+intervals = can.save_aps_to_txt(separating_path, action_potentials, time, voltage)
+# >
+phase_4_speed_list = []
+phase_0_speed_list = []
+num_of_APs = []
+radius_list = []
+x_y_list = []
+
+x_list = []
+y_list = []
+
+for number, ap in enumerate(action_potentials):
+    # <
+    phase_4_speed, phase_0_speed = can.find_voltage_speed(ap, time, voltage)
+    # >
+    # <
+    # if math.isnan(phase_4_speed):
+    #     phase_4_speed = can.replace_nan_with_nearest(phase_4_speed_list, number)
+    # if math.isnan(phase_0_speed):
+    #     phase_0_speed = can.replace_nan_with_nearest(phase_0_speed_list, number)
+
+    phase_4_speed = round(phase_4_speed, 3)
+    phase_0_speed = round(phase_0_speed, 3)
+
+    phase_4_speed_list.append(phase_4_speed)
+    phase_0_speed_list.append(phase_0_speed)
+    num_of_APs.append(number + 1)
+    # >
+
+    # <
+    current_ap_time = time[ap['pre_start']:ap['end']]
+    current_ap_voltage = voltage[ap['pre_start']:ap['end']]
+
+    radius, x, y = can.circle(current_ap_time, current_ap_voltage, limit_rad)
+    # if math.isnan(radius):
+    #     radius = can.replace_nan_with_nearest(radius_list, number)
+
+    # if math.isnan(x):
+    #     x = can.replace_nan_with_nearest(radius_list, number)
+
+    # if math.isnan(y):
+    #     y = can.replace_nan_with_nearest(radius_list, number)
+
+    x_list.append(x)
+    y_list.append(y)
+
+    radius_list.append(round(radius, 3))
+    x_y_list.append([round(x, 3), round(y, 3)])
+    # >
+
+print(intervals)
+print(separating_path)
+print(phase_0_speed_list)
+print(phase_4_speed_list)
+print(num_of_APs)
+print(radius_list)
+print(x_y_list)
---
+++ Potentials/bin/Debug/PythonScripts/Table_to_C#.py
@@ -0,0 +1,53 @@
+import atypic_cardio_analyzer as can
+import os
+import sys
+import shutil
+import warnings
+import math
+from concurrent.futures import ProcessPoolExecutor
+import dask.dataframe as dd
+
+# ----------------------------------------------------------------------------------------------
+
+C_sharp_data = sys.argv[1]
+# C_sharp_data = "D:/programming/projects/py/source/long_sample_data2019.txt"
+
+lines = C_sharp_data.split('\n')
+destination = lines[0]
+file_path = lines[1]
+
+inp_alpha_threshold = float(lines[2].replace(',', '.'))
+inp_start_offset = int(lines[3])
+inp_refractory_period = int(lines[4])
+limit_rad = float(lines[5].replace(',', '.'))
+"""
+file_path = C_sharp_data
+inp_alpha_threshold = 0.9
+inp_start_offset = 0
+inp_refractory_period = 280
+limit_rad = 250
+destination = "D:/programming/projects/2019long.xlsx"
+"""
+warnings.filterwarnings("ignore")
+
+phase_4_speed_list = []
+phase_0_speed_list = []
+num_of_APs = []
+radius_list = []
+
+# time, voltage = open_txt(file_path)
+time, voltage = can.preprocess(file_path, step=4)
+
+# Резка ПД
+action_potentials = can.find_action_potentials(time, voltage, alpha=inp_alpha_threshold,
+                                            refractory_period=inp_refractory_period, offset=inp_start_offset)
+
+# Сохранение в табличку
+try:
+    can.save_aps_to_xlsx(destination, action_potentials, time, voltage, limit_rad)
+    print("true")
+    print("0")
+
+except Exception as e:
+    print("false")
+    print(f"{type(e).__name__}: {str(e)}")
